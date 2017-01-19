7fc182e17a7f23522a7bfb5b8f2e393dedcbd591
Rasmus Ricks Sandberg <RRS@Rasmuss-MacBook-Pro.local>
2017-01-19 12:37:09 +0100

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Rasmus Ricks Sandberg on 19/01/2017.
+//  Copyright © 2017 Rasmus Ricks Sandberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
